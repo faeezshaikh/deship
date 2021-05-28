62bd081351ce48bb0ca85de2b3de9997b99641ae
Faeez Shaikh <faeez.shaikh@macys.com>
2021-05-28 15:30:24 -0500

    integrated moralis

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,4 +1,8 @@
+
 import { Component, OnInit } from '@angular/core';
+declare function signup(username,pass,email): void;
+declare let Moralis;
+
 
 @Component({
   selector: 'app-signup',
@@ -9,31 +13,50 @@ export class SignupPage implements OnInit {
   username;
   password;
   email;
-  constructor() { }
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
-  signmeup(){
+async signmeup(){
     console.log('username:' ,this.username);
     console.log('password:' ,this.password);
     console.log('email:' ,this.email);
 
     const user = new Moralis.User();
-    user.set("username", "my name");
-    user.set("password", "my pass");
-    user.set("email", "email@example.com");
+    user.set('username', this.username);
+    user.set('password', this.password);
+    user.set('email', this.email);
 
-    // other fields can be set just like with Moralis.Object
-    user.set("phone", "415-392-0202");
+
+
+
     try {
       await user.signUp();
       // Hooray! Let them use the app now.
     } catch (error) {
       // Show the error message somewhere and let the user try again.
-      alert("Error: " + error.code + " " + error.message);
+      console.log('Error: ' + error.code + ' ' + error.message);
+
     }
 
+
   }
+
+  async  login() {
+    let user = Moralis.User.current();
+    if (!user) {
+      user = await Moralis.Web3.authenticate();
+    }
+    console.log('logged in user:', user);
+}
+
+async  logOut() {
+    await Moralis.User.logOut();
+    console.log('logged out');
+}
+
 
 }
