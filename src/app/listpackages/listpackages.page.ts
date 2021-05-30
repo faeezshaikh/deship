6ee95f9dc0d9bd1035cb62b2864087daf03fe2c5
Faeez Shaikh <faeez.shaikh@macys.com>
2021-05-30 12:10:12 -0500

    Add pkgs form

--- src/app/listpackages/listpackages.page.ts
+++ src/app/listpackages/listpackages.page.ts
@@ -1,6 +1,7 @@
 
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MoralisService } from '../services/moralis.service';
 declare let Moralis;
 
 @Component({
@@ -15,9 +16,9 @@ export class ListpackagesPage implements OnInit {
 
   isLoading = false;
 
-  constructor(private router : Router) {
+  constructor(private router : Router,private moralisService: MoralisService) {
 
-    this.retrieveList();
+    this.retrieveList2();
    }
 
   ngOnInit() {
@@ -26,60 +27,11 @@ export class ListpackagesPage implements OnInit {
     this.router.navigateByUrl('/addpackage');
   }
 
-
-  async retrieveList() {
-
-    this.isLoading = true;
-    const query = new Moralis.Query('Package');
-
-
-    const results = await query.find();
-    this.isLoading = false;
-    console.log('Successfully retrieved ' + results.length + ' packages.');
-
-    for (let i = 0; i < results.length; i++) {
-      const object = results[i];
-      console.log(object.id + ' - Sender: ' + object.get('sender') + ' Receiver: ' + object.get('receiverAddr'));
-      this.packageList.push(
-
-        {'id':object.id,'sender':object.get('sender'),'senderAddr':object.get('senderAddr'),
-        'senderPhone':object.get('senderPhone'),'senderEmail':object.get('senderEmail'),
-      'receiverAddr':object.get('receiverAddr'),'receiverPhone':object.get('receiverPhone'),
-    'receiverEmail':object.get('receiverEmail'),'gems':object.get('gems'),'miles':object.get('miles'),
-  'img':object.get('img'),'fragile':object.get('fragile'),'confirm':object.get('confirm'),
-'instructions':object.get('instructions'),'status':object.get('status')}
-);
-    }
-
-    console.log('Array:',this.packageList);
-
-
+  async retrieveList2(){
+    this.packageList = await this.moralisService.retrieveList();
   }
 
-  addToList() {
-    const Package = Moralis.Object.extend('Package');
-    const pkg = new Package();
-    pkg.set('sender', 'fsh');
-    pkg.set('senderAddr', '1093 creek rd');
-    pkg.set('senderPhone', '374-434-3355');
-    pkg.set('senderEmail', 'fsh@gmail.com');
-    pkg.set('receiverAddr', '345 oak st.');
-    pkg.set('recieverPhone', '345-335-9734');
-    pkg.set('gems', 40);
-    pkg.set('miles', 8);
-    pkg.set('img', 'http://www.book-cycle.org/wp-content/uploads/2015/08/parcel.png');
-    pkg.set('fragile', false);
-    pkg.set('instructions', 'Drop at door');
-    pkg.set('confirm', false);
-    pkg.set('status', 'Open');
 
-
-    pkg.save().then((savedObject) => {
-      console.log('Successfully added :',savedObject);
-    }, (error) => {
-      console.log('Error retrieve:',error);
-    });
-  }
 
   removeFromList(id) {
 
